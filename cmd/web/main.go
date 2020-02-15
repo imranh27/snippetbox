164d36d64fb7acb46e4f965adece8446d51786bd
imranh27 <imranhashmi27@gmail.com>
2020-02-15 12:11:52 +0000

    Added CSS and made the UI look nicer. Chapter 2.9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,11 +16,15 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Print("Starting server on :4000")
 
 	fmt.Println("http://localhost:4000")
 	fmt.Println("http://localhost:4000/snippet?id=123")
 	fmt.Println("http://localhost:4000/snippet/create")
+	fmt.Println("http://localhost:4000/static/")
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
