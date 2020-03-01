835c047a19a6e43f5a393f57e9f5332df7e20678
imranh27 <imranhashmi27@gmail.com>
2020-03-01 10:50:10 +0000

    Added custom date formatter for templates. Chapter 5.6

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,6 +4,7 @@ import (
 	"github.com/imranh27/snippetbox/pkg/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 //define a templateData type to act as the holding structure for any holding data to pass to our HTML templates.
@@ -11,6 +12,16 @@ type templateData struct {
 	CurrentYear int
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+//Create humanDate function
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 @ 15:04")
+}
+
+//Imnitialise a template.FuncMap object and store in a global variable.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 //Build a template cache
@@ -31,8 +42,8 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		//Extract the name of the page
 		name := filepath.Base(page)
 
-		//Parse page template file in to template set.
-		ts, err := template.ParseFiles(page)
+		//Parse page template file in to template set. Must register functions before use.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
